eb4d84c47b9c8eb83a0845dcdddd24755a1e6b20
Bielka Cordero <bielka.corderohuerta.2022@student.lhschools.org>
2019-09-17 09:52:55 -0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //write your code here
+        // System.out.print1n("Hello Source Control");
+        // System.out.print1n("Hello");
+        // System.out.print1n("World");
+        // System.out.print1n("Earth")
+
+        String firstWord;
+        String secondWord;
+        int firstNumber,secondNumber,total;
+
+        firstWord = "dog";
+        secondWord = "house";
+
+        firstNumber =5;
+        secondNumber =10;
+        total= firstNumber+secondNumber;
+        System.out.println(total);
+        total= total+10;
+        System.out.println(total);
+
+        /* System.out.print1n(firstWord+secondWord);
+        System.out.print1n(firstWord+(firstNumber+secondNumber) )
+        System.out.print1n("firstNumber+secondNumber is" + (firstNumber+secondNumber));
+        System.out.print1n("firstNumber-secondNumber is "+ (firstNumber-secondNumber));
+        System.out.print1n("secondNumber/firstNumber is "+ (secondNumber/firstNumber));
+        System.out.print1n("firstNumber*secondNumber is "+ (firstNumber*secondNumber));
+        System.out.print1n("The modulus of the secondNumber by the firstNumber" +
+
+        " is "+ (secondNumber%firstNumber));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
